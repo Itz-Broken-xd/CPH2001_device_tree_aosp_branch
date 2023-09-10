daaa2cc7f0606b9102d82133f2ba51c2925bc762
Itz-Broken-xd <127822226+Itz-Broken-xd@users.noreply.github.com>
2023-09-10 10:28:52 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_CPH2001-user
-add_lunch_combo omni_CPH2001-userdebug
-add_lunch_combo omni_CPH2001-eng
+add_lunch_combo twrp_CPH2001-user
+add_lunch_combo twrp_CPH2001-userdebug
+add_lunch_combo twrp_CPH2001-eng
