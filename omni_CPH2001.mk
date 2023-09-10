acd6d976748ab5bc14029b74ee8bd6b9e4b02e80
Itz-Broken-xd <127822226+Itz-Broken-xd@users.noreply.github.com>
2023-09-10 10:57:59 +0530

    Update omni_CPH2001.mk

--- omni_CPH2001.mk
+++ omni_CPH2001.mk
@@ -7,10 +7,10 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from CPH2001 device
 $(call inherit-product, device/oppo/CPH2001/device.mk)
